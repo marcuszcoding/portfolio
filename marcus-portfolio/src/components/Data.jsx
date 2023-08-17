79d93f57d4bfc8f083dc3929b910385a64ff826e
marcuszcoding <marcuszcoding@gmail.com>
2023-08-17 12:06:57 -0600

    add files

--- marcus-portfolio/src/components/Data.jsx
+++ marcus-portfolio/src/components/Data.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Data = () => {
   return (
-    <div>Data</div>
+    <div className="home-data">
+      <h1 className="home-title">Marcus Zimmer</h1>
+    </div>
   )
 }
 
