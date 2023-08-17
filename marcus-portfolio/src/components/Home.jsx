291f3b856a0ec4e9024548abcd76895d2f9723d5
marcuszcoding <marcuszcoding@gmail.com>
2023-08-17 09:32:32 -0600

    initial commit

--- marcus-portfolio/src/components/Home.jsx
+++ marcus-portfolio/src/components/Home.jsx
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home-container container grid"></div>
+        <div className="home-content grid">
+            
+        </div>
     </section>
   )
 }
