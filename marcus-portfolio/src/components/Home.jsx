e00d7d2de060cbf09ac79edde0a1bec5eab749be
marcuszcoding <marcuszcoding@gmail.com>
2023-08-17 11:06:22 -0600

    add social component

--- marcus-portfolio/src/components/Home.jsx
+++ marcus-portfolio/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import "./home.css"
+import Social from './Social'
 
 const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home-container container grid"></div>
         <div className="home-content grid">
-            
+            <Social />
         </div>
     </section>
   )
--- marcus-portfolio/src/components/Social.jsx
+++ marcus-portfolio/src/components/Social.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Social = () => {
   return (
-    <div>Social</div>
+    <div className="home-social">
+      <a href="https://www.instagram.com/marcus_zimmer_/" className="home-social-icon" target={"_blank"}>
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a href="https://github.com/marcuszcoding" className="home-social-icon" target={"_blank"}>
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
   )
 }
 
